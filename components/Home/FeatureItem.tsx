05ecb338683b415fc10103de1fb717a022f3edcb
Morshed <morshedmto@gmail.com>
2024-08-01 23:06:11 +0600

    type error fix

--- components/Home/FeatureItem.tsx
+++ components/Home/FeatureItem.tsx
@@ -1,7 +1,14 @@
 // components/FeatureItem.js
-import React from 'react';
+import React, { ReactNode } from "react";
 
-const FeatureItem = ({ icon, title, description, link }) => {
+type Props = {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  link: string;
+};
+
+const FeatureItem = ({ icon, title, description, link }: Props) => {
   return (
     <div className="max-w-md sm:mx-auto sm:text-center">
       <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50 sm:mx-auto sm:w-24 sm:h-24">
