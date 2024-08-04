052429534eef6b37bd6cf9559b840f07a00c37eb
Morshed <morshedmto@gmail.com>
2024-08-04 22:57:09 +0600

    loader changed

--- app/(client)/loading.tsx
+++ app/(client)/loading.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
 import { grid } from "ldrs";
-
-grid.register("l-grid");
 
 // Default values shown
 
 const loading = () => {
-    useEffect(() => {
-        async function getLoader() {
-          const { spiral } = await import('ldrs')
-          spiral.register()
-        }
-        getLoader()
-      }, [])
-      return <div className="h-[calc(100vh-64px)] flex items-center justify-center">
-        <l-grid color="#2563EB"></l-grid>
-      </div>
+  useEffect(() => {
+    async function getLoader() {
+      const { grid } = await import("ldrs");
+      grid.register();
+    }
+    getLoader();
+  }, []);
+  return (
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <l-grid color="#2563EB"></l-grid>
+    </div>
+  );
 };
 
 export default loading;
